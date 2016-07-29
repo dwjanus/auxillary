2f5c7af82a92cc56ecc342da4c42bfe035a125fe
dwjanus <devin.janus@samanage.com>
2016-07-29 13:07:29 -0400

    bot temporarily commented out

--- slackmanage.rb
+++ slackmanage.rb
@@ -3,18 +3,18 @@ require 'httparty'
 require 'json'
 require 'slack-ruby-bot'
  
-# get '/anonymize' do
-#   content_type :json
-#   {:text => params[:text], :response_type => "in_channel"}.to_json
-# end
-
-class SamanageBot < SlackRubyBot::Bot
-  command 'ping' do |client, data, match|
-    client.say(text: 'pong', channel: data.channel)
-  end
+get '/anonymize' do
+  content_type :json
+  {:text => params[:text], :response_type => "in_channel"}.to_json
 end
 
-SamanageBot.run 
+# class SamanageBot < SlackRubyBot::Bot
+#   command 'ping' do |client, data, match|
+#     client.say(text: 'pong', channel: data.channel)
+#   end
+# end
+
+# SamanageBot.run 
 
 
 
