851f8bb5e86b09cf6df436dd0071a31d179f5516
dwjanus <devin.janus@samanage.com>
2016-07-29 00:14:21 -0400

    should work now pt 3

--- slackmanage.rb
+++ slackmanage.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'httparty'
 require 'json'
  
-get '/anonymize' do
+get '/anon' do
   content_type :json
   {:text => params[:text], :response_type => "in_channel"}.to_json
 end
