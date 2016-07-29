76741ea0f192775edaf104d74f5f7f9a557c6e83
dwjanus <devin.janus@samanage.com>
2016-07-29 00:07:15 -0400

    should work now pt 2

--- slackmanage.rb
+++ slackmanage.rb
@@ -3,13 +3,9 @@ require 'httparty'
 require 'json'
  
 get '/anonymize' do
-  postback params[:text], params[:channel_id]
-  status 200
+  content_type :json
+  {:text => params[:text], :response_type => "in_channel"}.to_json
 end
- 
-def postback message, channel
-    slack_webhook = ENV['https://hooks.slack.com/services/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']
-    HTTParty.post slack_webhook, body: {"text" => message.to_s, "username" => "John Doe", "channel" => params[:channel_id]}.to_json, headers: {'content-type' => 'application/json'}
-end
+
 
 
